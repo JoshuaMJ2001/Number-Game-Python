ee1962df9e1897de2245ad8774fdda3f8ceb9e9c
Joshua Martin - Jones <joshua.martin-Jones@bourne-grammar.lincs.sch.uk>
2016-11-08 18:23:30 +0000

    Updated Code - Completed Menu
    Added all the menu options to the code, created function names.

--- code.py
+++ code.py
@@ -6,7 +6,29 @@ def mainMenu():
 		print('''Welcome to the Menu - What would you like to do?
 (1) Roll One Dice
 (2) Roll Two Die
-(3) Flip a Coin
-(4) Number Between 0 and 100
-(5) Pick A Card
-(6) Quit''')
+(3) Flip and Guess a Coin
+(4) Guess a Random Number Between 1 and 100
+(5) Guess a Random Number Between 1 and 100
+(6) Guess a Random Number Between 1 and 100
+(7) Pick A Card
+(8) Quit''')
+		if choice == 1:
+			dice()
+        elif choice == 2:
+            die()
+        elif choice == 3:
+            coin()
+        elif choice == 4:
+            number_10()
+        elif choice == 5:
+            number_100()
+        elif choice == 6:
+            number_1000
+		elif choice == 7:
+            card()
+		elif choice == 8:
+            running = False
+        else:
+            menu()
+
+mainMenu()
