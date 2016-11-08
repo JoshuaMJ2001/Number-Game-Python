f7e045c94f38fc32f850465a87bec0401ec04d4b
Joshua Martin - Jones <joshua.martin-Jones@bourne-grammar.lincs.sch.uk>
2016-11-08 18:13:17 +0000

    Code

---
+++ code.py
@@ -0,0 +1,12 @@
+import random
+
+def mainMenu():
+	running = True
+	while running == True:
+		print('''Welcome to the Menu - What would you like to do?
+(1) Roll One Dice
+(2) Roll Two Die
+(3) Flip a Coin
+(4) Number Between 0 and 100
+(5) Pick A Card
+(6) Quit''')
